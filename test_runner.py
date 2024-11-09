f823d232b59b198416487c7d3e2fb91d3745398f
Alex-1000 <neijr@proton.me>
2024-11-09 07:37:20 +0300

    Updated unit tests

--- test_runner.py
+++ test_runner.py
@@ -6,13 +6,13 @@ import subprocess
 
 command, *arg = shlex.split("".join(sys.argv[1:]))
 arg = "".join(arg)
-output = bytes(
-    "".join(c for c in arg if not c.isnumeric()).replace("+", "++").replace("-", "--"),
-    encoding="UTF-8",
-)
+output = "".join(c for c in arg if not c.isnumeric()).replace("+", "++").replace("-", "--")
+output = bytes(f"{output}\n{output}\n", encoding="UTF-8")
 result = subprocess.run([command, arg], capture_output=True)
-if output not in result.stdout:
+if result.stdout != output:
     print(
+        "Expected output:",
+        output,
         "Test has not passed. Program output:",
         result.stdout,
         "Error log:",
